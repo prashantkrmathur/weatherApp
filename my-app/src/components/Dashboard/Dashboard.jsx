d783bd42b69970ca54ce00539f1b9c9c8cf373c8
Prashant Kumar <prashantkrmathur@gmail.com>
2022-07-30 12:41:03 +0530

    Added the SearchInput box

---
+++ my-app/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  "./Dashboard.css"
+const Dashboard = () => {
+    return (<div className="align center" style={{ margin: "auto", width: "700px" }}>
+        <div>
+          <input className='input-style' type="text"  placeholder='Enter the city Name'/>
+      </div>
+      </div>
+  )
+}
+
+export default Dashboard
